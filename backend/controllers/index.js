290631baf655d65417ef1ccce2a803fd802fdb14
Soichi Hayashi <Soichi Hayashi>
2020-03-07 22:09:20 -0500

    added covid19 twitter map

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const jwt = require('express-jwt');
 const async = require('async');
+const fs = require('fs');
 
 const config = require('../config');
 
@@ -149,6 +150,106 @@ router.get('/bvi/:statefips/:countyfips', async (req, res, next)=>{
     res.json(recs);
 });
 
+router.get('/covid19/tweets/states/:block', async (req, res, next)=>{
+    let block = req.params.block;
+    if(!block.match(/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/)) return next("bad block");
+    let path = "/home/hayashis/git/apred/data/covid19/"+block+"/tweets_state.csv";
+    let csv = fs.readFileSync(path, "ascii");
+    let lines = csv.split("\n");
+    let headers = lines.shift();
+    let data = [];
+    lines.forEach(line=>{
+        let cols = line.split(",");
+        data.push({
+            state: cols[0], 
+            total: parseInt(cols[1]),
+            virus: parseInt(cols[2]),
+            virus_p: parseFloat(cols[3]),
+        });
+    });
+    res.json(data);
+});
+
+const geocode = require("/home/hayashis/git/apred/data/geocode.json");
+//store in dict of state/city for quick lookup
+const geocode_lookup = {};
+geocode.forEach(city=>{
+    geocode_lookup[city.state_code+"/"+city.city] = [city.longitude, city.latitude];
+});
+
+//preload blocks
+const tweets_city_20200307 = loadTweetsCity("/home/hayashis/git/apred/data/covid19/2020-03-07/tweets_city.csv");
+console.log("done loading", tweets_city_20200307.features[0]);
+    
+function loadTweetsCity(path) {
+    console.log("loading", path);
+    let csv = fs.readFileSync(path, "ascii");
+    let lines = csv.split("\n");
+    let headers = lines.shift();
+
+    let features = [];
+
+    //parse csv
+    lines.forEach(line=>{
+        if(line == "") return;
+        let cols = line.split(",");
+
+        let city = cols[0];
+        let state_code = cols[1];
+        let total = parseInt(cols[2]);
+        let total_user = parseInt(cols[3]);
+        let total_p = parseFloat(cols[4]);
+        let virus = parseFloat(cols[5]);
+        let user_virus = parseFloat(cols[6]);
+        let virus_p = parseFloat(cols[7]);
+        let virus_rel_p = parseFloat(cols[8]);
+        let user_virus_p = parseFloat(cols[9]);
+
+        //lookup geocode
+        let coordinates = geocode_lookup[state_code+"/"+city];
+        if(!coordinates) {
+            console.error("unknown city", state_code, city);
+            return;
+        }
+
+
+        features.push({
+            type: "Feature",
+            geometry: {
+                type: "Point",
+                coordinates,
+            },
+            properties: {
+                city,state_code,total,total_user,total_p,virus,user_virus,virus_p,virus_rel_p,user_virus_p
+            }
+        });
+
+        if(city == "San Antonio") {
+            console.log("San Antonio dump");
+            console.dir(features[features.length-1]);
+        }
+    });
+
+    return {
+        type: "FeatureCollection",
+        features,
+    }
+}
+
+
+//we need to convert a large geojson to vector layer using a tool like..
+//https://github.com/mapbox/geojson-vt
+router.get('/covid19/tweets/cities/:block', async (req, res, next)=>{
+    let block = req.params.block;
+    if(!block.match(/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/)) return next("bad block");
+    switch(block) {
+    case "2020-03-07":
+        res.json(tweets_city_20200307);
+        break;
+    default:
+        next("no found");
+    }
+});
 /*
 router.use('/project', require('./project'));
 router.use('/dataset', require('./dataset'));
