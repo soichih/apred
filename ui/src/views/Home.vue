afdd560afff4a3507300c87f160a2d4090aabf51
Soichi Hayashi <soichih@gmail.com>
2020-03-09 11:22:15 -0400

    adding ui script

---
+++ ui/src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="page">
+    <br>
+    <router-link to="/disasters">Disaster Declarations</router-link>
+    <br>
+    <br>
+    <router-link to="/covid19">COVID19</router-link>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+    name: 'Home',
+    components: {},
+
+}
+</script>
+
+<style scoped>
+.home {
+  padding: 20px;
+}
+a {
+  color: gray;
+}
+</style>
