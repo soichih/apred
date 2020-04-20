e771b1387c9c06fe4b4cd017e341a803040749d9
Soichi Hayashi <hayashis@iu.edu>
2020-04-19 22:23:22 -0400

    added storm event graph

--- ui/src/components/Eligibility2019.vue
+++ ui/src/components/Eligibility2019.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
     <br>
-    <el-button type="success" plain @click="showDetail = true" v-if="!showDetail" style="width: 100%">
+    <el-button type="success" plain @click="showDetail = true" v-if="!showDetail">
         <i class="el-icon-caret-right"/> This incident maybe eligible for 2019 disaster supplemental funding
     </el-button>
     <div v-if="showDetail">
