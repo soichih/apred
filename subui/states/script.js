23c5739643e0732ddd07769940e3afcdf28e66a0
Soichi Hayashi <hayashis@iu.edu>
2020-05-01 20:56:15 -0400

    updated data url for restriction map

--- subui/states/script.js
+++ subui/states/script.js
@@ -209,12 +209,13 @@ new Vue({
     methods: {
         loadGeo() {
             return new Promise((resolve, reject)=>{
-                axios.get("covid19states.geojson").then(res=>{
+                axios.get("https://gpu1-pestillilab.psych.indiana.edu/apred/covid19states.geojson").then(res=>{
                     resolve(res.data);
                 });
             });
         },
 
+            /*
         loadCounty() {
             return new Promise((resolve, reject)=>{
                 axios.get("travelrestriction.json").then(res=>{
@@ -223,10 +224,11 @@ new Vue({
                 });
             });
         },
+            */
 
         loadCSV() {
             return new Promise((resolve, reject)=>{
-                axios.get("COVID2019StateTrackingChart.csv").then(res=>{
+                axios.get("https://gpu1-pestillilab.psych.indiana.edu/apred/COVID2019StateTrackingChart.csv").then(res=>{
                     //csv contains &nbsp; for space.. I need to replace it with space so we can compare names
                     this.modified = res.headers["last-modified"];
                     let re = new RegExp(String.fromCharCode(160), "g");
