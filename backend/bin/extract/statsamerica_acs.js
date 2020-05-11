3a92994af86626174df6d8dae73d743a1834e6e7
Soichi Hayashi <hayashis@iu.edu>
2020-05-11 18:16:37 -0400

    added an extractor for statsamerica_acs(tables) and bea_gdp added gdp(?) data to county json

---
+++ backend/bin/extract/statsamerica_acs.js
@@ -0,0 +1,62 @@
+#!/usr/bin/env nodejs
+
+const fs = require('fs');
+const async = require('async');
+const config = require('../../config');
+const mssql = require('mssql');
+
+//I can only connect from IU VPN connected IPs - not dev1
+console.log("connecting to db --------------------------");
+mssql.connect(config.stats_america.db_stats_dms5).then(pool=>{
+    load(pool);
+});
+
+function load(pool) {
+    async.series([
+        next=>{
+            console.log("loading acs.medianincome --------------------------");
+            pool.request().query(`SELECT * FROM acs_common_items_extract WHERE code_id = '307'`).then(res=>{
+                fs.writeFileSync(__dirname+"/../../../raw/statsamerica.acs.medianincome.json", JSON.stringify(res.recordset));
+                next();
+            });
+        },
+
+        next=>{
+            console.log("loading acs.poverty_universe --------------------------");
+            pool.request().query(`SELECT * FROM acs_common_items_extract WHERE code_id = '385'`).then(res=>{
+                fs.writeFileSync(__dirname+"/../../../raw/statsamerica.acs.poverty_universe.json", JSON.stringify(res.recordset));
+                next();
+            });
+        },
+        
+        next=>{
+            console.log("loading acs.poverty_individuals --------------------------");
+            pool.request().query(`SELECT * FROM acs_common_items_extract WHERE code_id = '364'`).then(res=>{
+                fs.writeFileSync(__dirname+"/../../../raw/statsamerica.acs.poverty_individuals.json", JSON.stringify(res.recordset));
+                next();
+            });
+        },
+
+        next=>{
+            console.log("loading acs.under18 --------------------------");
+            pool.request().query(`SELECT * FROM acs_common_items_extract WHERE code_id = '411'`).then(res=>{
+                fs.writeFileSync(__dirname+"/../../../raw/statsamerica.acs.under18.json", JSON.stringify(res.recordset));
+                next();
+            });
+        },
+
+        next=>{
+            console.log("loading acs.over65 --------------------------");
+            pool.request().query(`SELECT * FROM acs_common_items_extract WHERE code_id = '365'`).then(res=>{
+                fs.writeFileSync(__dirname+"/../../../raw/statsamerica.acs.over65.json", JSON.stringify(res.recordset));
+                next();
+            });
+        },
+    ], err=>{
+        if(err) throw err;
+        pool.close();
+    });
+}
+
+
+
