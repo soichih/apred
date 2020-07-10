93afa0ee26755a97062a5c6fc6150152bcfeca36
Soichi Hayashi <hayashis@iu.edu>
2020-07-10 18:09:39 -0400

    updated statsamerica_demo to load population info since 2009 - instead of just 2018

--- backend/bin/extract/COVID2019StateTrackingChart.py
+++ backend/bin/extract/COVID2019StateTrackingChart.py
@@ -13,15 +13,17 @@ import re
 
 # Find the current PDF url from webiste
 nga_url = "https://www.nga.org/coronavirus/"
+#https://www.nga.org/wp-content/uploads/2020/06/6-22_Summary-of-State-Actions-Addressing-Business-Reopenings.xlsx
 response = requests.get(nga_url, headers={'User-Agent':'Mozilla/5.0'})
 rgx = re.compile(r'https:\/\/www.nga.org\S*CoronavirusStateActionsChart\S*.xlsx')
 rgx_matches = rgx.findall(response.text)
-
 if len(rgx_matches) != 1:
     print("failed to find xlsx filename")
     sys.exit(1)
 
 url = rgx_matches[0]
+
+#url = "https://www.nga.org/wp-content/uploads/2020/06/CoronavirusStateActionsChart_19June2020.xlsx"
 
 # load the excel file
 response = requests.get(url, headers={'User-Agent':'Mozilla'})
