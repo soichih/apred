c718c287b9942b3069c3dd0beaa5cb8bcd78f6c7
Soichi Hayashi <hayashis@iu.edu>
2020-08-12 21:13:03 -0400

    made various backend updates on how to extract / transform statsamerica data

--- backend/bin/extract/statsamerica_dr.js
+++ backend/bin/extract/statsamerica_dr.js
@@ -10,28 +10,31 @@ console.log("statsamerica_resilience -----------------------------------");
 //I can only connect from IU VPN connected IPs - not dev1
 mssql.connect(config.stats_america.db_red_dr).then(async pool=>{
     console.log("loading dr_data / Resilience values for individual indices per county.");
+    const dr_data = await pool.request().query(`
+        SELECT * FROM dr_data
+    `);
+    dr_data.recordset.forEach(rec=>{
+        rec.statefips = rec.statefips.trim();
+        rec.countyfips = rec.countyfips.trim();
+        rec.measure = rec.measure.trim();
+        rec.measure_category = rec.measure_category.trim();
+        rec.year = rec.year.trim();
+
+        //if(rec.statefips == "18" && rec.countyfips == "105" && rec.measure == "16") console.dir(rec);
+    });
+    fs.writeFileSync(__dirname+"/../../../raw/dr.json", JSON.stringify(dr_data.recordset));
+
+    console.log("loading dr_data_norm / Resilience values for individual indices per county.");
     const dr_data_norm = await pool.request().query(`
         SELECT * FROM dr_data_norm
     `);
     dr_data_norm.recordset.forEach(rec=>{
-        /*
-        {
-          statefips: '51        ',
-          countyfips: '171       ',
-          measure: '11        ',
-          measure_category: '1         ',
-          year: '2016      ',
-          measure_value: 1.359882,
-          measure_value_normalized: null
-        },
-        */
         rec.statefips = rec.statefips.trim();
         rec.countyfips = rec.countyfips.trim();
         rec.measure = rec.measure.trim();
         rec.measure_category = rec.measure_category.trim();
         rec.year = rec.year.trim();
     });
-    //console.dir(dr_data_norm.recordset);
     fs.writeFileSync(__dirname+"/../../../raw/dr_normalized.json", JSON.stringify(dr_data_norm.recordset));
 
     console.log("loading dr_measure (will be populated in the future)");
@@ -52,34 +55,6 @@ mssql.connect(config.stats_america.db_red_dr).then(async pool=>{
     });
     fs.writeFileSync(__dirname+"/../../../raw/dr_measure_category.json", JSON.stringify(dr_measure_category.recordset));
 
-    /*
-
-    const supp_counties = await pool.request().query(`
-        SELECT * FROM eda_disaster_supp_counties;
-    `);
-    //console.dir(supp_counties.recordsets[0].length);
-
-    const supp_fema = await pool.request().query(`
-        SELECT * FROM eda_disaster_supp_fema;
-    `);
-    //console.dir(supp_fema.recordsets[0].length);
-
-    let fains = {};
-    supp.recordsets[0].forEach(rec=>{
-        rec.counties = [];
-        fains[rec.fain] = rec;
-    });
-    supp_counties.recordsets[0].forEach(rec=>{
-        fains[rec.fain].counties.push({county: rec.county, stateadd: rec.stateabb});
-    });
-    supp_fema.recordsets[0].forEach(rec=>{
-        fains[rec.fain].fema_id = rec.fema_id;
-    });
-
-    //console.dir(fains);
-    fs.writeFileSync(__dirname+"/../../../raw/statsamerica.eda2018.json", JSON.stringify(fains));
-    pool.close();
-    */
     pool.close();
 });
 
