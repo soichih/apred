86455790f8efb6c4d494ac36d4549e066afda9a5
Soichi Hayashi <hayashis@iu.edu>
2020-10-11 00:21:53 -0400

    replaced plotly graph for bvi

--- ui/src/components/CountySelecter.vue
+++ ui/src/components/CountySelecter.vue
@@ -1,6 +1,14 @@
 <template>
-<el-select v-model="myfips" filterable clearable placeholder="Search county to show detail" @change="$emit('select', myfips)" style="width: 100%;" size="large">
-    <el-option v-for="item in countyList" :key="item.value" :label="item.label" :value="item.value">
+<el-select 
+    v-model="myfips" 
+    filterable 
+    clearable 
+    remote
+    :remote-method="searchCounty"
+    :loading="loading"
+    placeholder="Search county to show detail" 
+    @change="change" style="width: 100%;" size="large">
+    <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value">
         <span style="float: left">{{ item.label }}</span>
         <span style="float: right; color: #8492a6; font-size: 13px">{{ item.value }}</span>
     </el-option>
@@ -15,6 +23,7 @@ export default {
     data() {
         return {
             countyList: null,
+            options: [],
             myfips: null,
             loading: false,
         }
@@ -36,6 +45,28 @@ export default {
     },
 
     methods: {
+        searchCounty(q) {
+            this.options = [];
+            if(q.length < 3) {
+                return;
+            }
+
+            console.log("selecting", q);
+            this.loading = true;
+            const lq = q.toLowerCase();
+            this.options = this.countyList.filter(o=>{
+                if(o.label.toLowerCase().includes(lq)) return true;
+                if(o.value.toString().includes(lq)) return true;
+                return false;
+            });
+            //if(this.options.length > 30) this.options = this.options.slice(0, 30);
+            this.loading = false;
+        },
+
+        change() {
+            this.$emit('select', this.myfips);
+            this.myfips = null;
+        }
     }
 }
 </script>
