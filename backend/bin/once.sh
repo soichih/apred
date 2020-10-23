9b58db18063085b7922deea90735594625f92e70
Soichi Hayashi <hayashis@iu.edu>
2020-10-23 14:40:56 -0400

    reverted a script to load past noaa storm counts

--- backend/bin/once.sh
+++ backend/bin/once.sh
@@ -3,6 +3,7 @@
 ./extract/fips.js
 ./extract/county_geojson.js
 ./extract/statsamerica_bea_gdp.js
+./extract/statsamerica_noaa_storms_past.js
 
 ./transform/find_nearest_tribes.js
 ./transform/count_noaa_storms.js
