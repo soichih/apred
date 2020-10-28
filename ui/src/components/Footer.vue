b2ad727f91f93e6b384e30328c90e601cca053ea
Soichi Hayashi <soichih@gmail.com>
2020-10-28 17:29:52 -0400

    rebuilt ui dist

--- ui/src/components/Footer.vue
+++ ui/src/components/Footer.vue
@@ -6,7 +6,7 @@
             disaster mitigation, analysis, and recovery activities. 
         </p>
         <p>
-            <i>This publication was prepared by the CTIL and IBRC at Indiana University using Federal funds awarded to the Trustees of Indiana University and as a sub-component under award number ED17HDQ3120040 from the U.S. Economic Development Administration, U.S. Department of Commerce. The statements, findings, conclusions, and recommendations are those of the author(s) and do not necessarily reflect the views of the Economic Development Administration or the U.S. Department of Commerce.</i>
+            <i>This publication was prepared by the CTIL and IBRC at Indiana University using federal funds awarded to the Trustees of Indiana University and as a sub-component under award number ED17HDQ3120040 from the U.S. Economic Development Administration, U.S. Department of Commerce. The statements, findings, conclusions, and recommendations are those of the author(s) and do not necessarily reflect the views of the Economic Development Administration or the U.S. Department of Commerce.</i>
         </p>
         <div style="float: right">
             <a href="mailto:ctil@iu.edu">ctil@iu.edu</a> | <a href="mailto:ctil@iu.edu">Contact Us</a>
