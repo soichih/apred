6429f361c9a092f6e9a5a5cffc9c9822d1207a62
Soichi Hayashi <hayashis@iu.edu>
2020-12-08 22:04:09 -0500

    updated eda2018 to output data in geojson

--- backend/bin/transform/county_geo2albersusa.sh
+++
@@ -1,6 +0,0 @@
-#https://www.mapbox.com/elections/albers-usa-projection-style
-#https://github.com/developmentseed/dirty-reprojectors
-
-
-cd ~/git/apred/raw
-cat counties_geo.json | dirty-reproject --forward albersUsa > counties_geo.albers.json
