99a82c0e35db751bb0263295b284025807f7307f
Soichi Hayashi <hayashis@iu.edu>
2021-02-02 15:27:41 -0500

    added scripts to extract / transform statsamerica eda_regions

--- backend/bin/once.sh
+++ backend/bin/once.sh
@@ -5,7 +5,8 @@
 ./extract/statsamerica_bea_gdp.js
 ./extract/statsamerica_noaa_storms_past.js
 ./extract/statsamerica_fema_disasters_past.js
+./extract/statsamerica_resions.js
 
 ./transform/find_nearest_tribes.js
 ./transform/count_noaa_storms.js
-
+./transform/regions.js
