cf359782713b6579f0652bea469307a66d8e164a
Soichi Hayashi <hayashis@iu.edu>
2021-02-20 14:25:46 -0500

    fixed typo

--- backend/bin/daily.sh
+++ backend/bin/daily.sh
@@ -27,7 +27,7 @@
 pubdir="/pub"
 
 #debug..
-[ $(hostname)" == "gpu1-pestillilab.psych.indiana.edu" ] pubdir=/home/hayashis/git/apred/pub
+[ "$(hostname)" == "gpu1-pestillilab.psych.indiana.edu" ] && pubdir=/home/hayashis/git/apred/pub
 
 cat $pubdir/counties_geo.json | dirty-reproject --forward albersUsa > $pubdir/counties_geo.albers.geojson
 cat $pubdir/eda2018.geojson | dirty-reproject --forward albersUsa > $pubdir/eda2018.albers.geojson
